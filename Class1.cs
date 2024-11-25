82ea2a2ff2127f22d4ffa9f15098582fb8d78b5e
rayyanmohsin1 <rmohsin@my.centennialcollege.ca>
2024-11-25 13:44:21 +0500

    first commit

---
+++ Class1.cs
@@ -0,0 +1,14 @@
+﻿namespace StringExtensionLibrary
+{
+    public static class StringExtensions
+    {
+        public static bool StartsWithUpperCase(this string input)
+        {
+            if (string.IsNullOrEmpty(input))
+                return false;
+
+            char firstChar = input[0];
+            return char.IsUpper(firstChar);
+        }
+    }
+}
